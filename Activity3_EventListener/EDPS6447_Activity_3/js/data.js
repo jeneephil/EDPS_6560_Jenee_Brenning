f0d1cc6b3857fd2c9216fbb041063866ae39b00a
jeneephil <59579316+jeneephil@users.noreply.github.com>
2020-07-30 10:33:41 -0600

    Add files via upload

---
+++ Activity3_EventListener/EDPS6447_Activity_3/js/data.js
@@ -0,0 +1,34 @@
+var array_questions = [
+	{
+		statement: "Princess Leia's signature buns were nicknamed the 'Hot Plate Special' and were only approved after Lucas was shown 40 different hairstyles.",
+		topic: "starwars",
+		sub_topic: "princess leia",
+		answer_key: "option1",
+		hint: "https://www.factinate.com/things/23-force-defying-facts-princess-leia/",
+        help: "Patience you must have my young Padawan.",
+        label: ["True","False","Maybe"],
+        video: {display: true, src: "https://giphy.com/embed/3o7abrH8o4HMgEAV9e"},
+	},
+	{
+		statement: "Carrie Fisher said in 2014 'I would rather have played Han Solo. When I first read the script I thought that's the part to be, always wry and sardonic. He's always that. I feel like a lot of the time Leia's either worried or pissed or, thank God, sort of snarky'.",
+		topic: "starwars",
+		sub_topic: "princess leia",
+		answer_key: "option1",
+		hint: "https://en.wikipedia.org/wiki/Princess_Leia",
+        help: "Pass on what you have learned.",
+        label: ["True","False","Maybe"],
+        video: {display: true, src:"https://giphy.com/embed/3ohuAxV0DfcLTxVh6w"},
+	},
+    {
+		statement: "'In Star Wars: The Force Awakens' (30 years after the return of the Jedi) the character Princess Leia was now referred to as General Leia, except for a slipup by one of the characters calling her princess.",
+		topic: "starwars",
+		sub_topic: "princess leia",
+		answer_key: "option1",
+		hint: "https://en.wikipedia.org/wiki/Princess_Leia",
+        help: "Do or do not. There is no try.",
+        label: ["True","False","Maybe"],
+        video: {display: false, src:""},
+	},
+];
+
+var array_answers = [];
